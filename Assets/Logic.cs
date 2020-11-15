c9d7aee986b49e7d4dd9635526c066377d2e10e7
unknown <assma.bm@gmail.com>
2020-11-15 20:16:20 +0200

    CODE REFACTOR: changed variable to private

--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -19,8 +19,7 @@ public class Logic : MonoBehaviour
     public float pausetime = 0.5f;
 
     public bool player = false;
-    public bool logic;
-    public bool robot = false;
+    private bool robot = false;
 
     // Start is called before the first frame update
     void Start()
