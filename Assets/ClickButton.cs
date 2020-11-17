26118331173f070cc6c0bfdc210a0ee03d7af033
unknown <assma.bm@gmail.com>
2020-11-17 22:41:18 +0200

    CODE REFACTOR: deleting voice getComponent cuz i wanna try another way of playing audio

--- Assets/ClickButton.cs
+++ Assets/ClickButton.cs
@@ -17,7 +17,6 @@ public class ClickButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audio = GetComponent<AudioSource>();
         renderer = GetComponent<Renderer>();
         renderer.enabled = true;
     }
